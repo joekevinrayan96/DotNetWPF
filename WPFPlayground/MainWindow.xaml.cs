a319b81807561433a7d284df14dca238b9232901
Joe Kevin Rayan <joekevinrayan1996@gmail.com>
2022-12-16 11:38:24 +1100

    commited only xaml, inheritance

--- WPFPlayground/MainWindow.xaml.cs
+++ WPFPlayground/MainWindow.xaml.cs
@@ -18,11 +18,40 @@ namespace WPFPlayground
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    class Car : Vehicle  // Derived class
+    {
+        public string modelName = "Mustang";  // Car field
+
+       
+    }
+
+    class Vehicle  // Base class
+    {
+        public string brand = "Ford";  // Vehicle field
+
+        
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            Car myCar = new Car();
+
+            // Call the honk() method (From the Vehicle class) on the myCar object
+            //myCar.honk();
+           
+
+            // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
         }
 
         private void submitButton_Click(object sender, RoutedEventArgs e)
@@ -30,7 +59,6 @@ namespace WPFPlayground
             //MessageBox.Show($"Hello {firstNameText.Text}");
             MessageBox.Show("Hello " + firstNameText.Text);
 
-            
         }
     }
 
